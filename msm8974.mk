112cd3ec25d98cf63aed8e64c93a9404e2a9c245
Ondrej Hlavac <hlavaco@gmail.com>
2017-07-11 12:23:24 +0200

    msm8974-common: Remove power init
    Rhine uses different devfreq governor. On msm8974ab it causes lags.
    
    Change-Id: Ic0f9ea7ceaefd701542016742808bdff58a76bc0
    Signed-off-by: Nikhil Punathil <nikhilpe@gmail.com>

--- msm8974.mk
+++ msm8974.mk
@@ -68,8 +68,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/rootdir/init.msm8974-common.rc:root/init.msm8974-common.rc \
     $(COMMON_PATH)/rootdir/init.sony.usb.rc:root/init.sony.usb.rc \
-    $(COMMON_PATH)/rootdir/init.recovery.qcom.rc:root/init.recovery.qcom.rc \
-    $(COMMON_PATH)/rootdir/init.qcom.power.rc:root/init.qcom.power.rc
+    $(COMMON_PATH)/rootdir/init.recovery.qcom.rc:root/init.recovery.qcom.rc
 
 
 PRODUCT_COPY_FILES += \
