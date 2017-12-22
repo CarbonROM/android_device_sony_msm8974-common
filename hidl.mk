037ca8fe6924745cfbb7297e749a5ba1cfaba1ef
Max Weffers <rcstar6696@gmail.com>
2017-12-22 14:56:18 +0100

    msm8974: add soundtrigger hidl hal
    Change-Id: I9f2d4d2046fb8406b8fe7f8073ad9dbf4a9e2f4d

--- hidl.mk
+++ hidl.mk
@@ -1,7 +1,8 @@
 # Audio
 PRODUCT_PACKAGES += \
 	android.hardware.audio@2.0-impl \
-	android.hardware.audio.effect@2.0-impl
+	android.hardware.audio.effect@2.0-impl \
+	android.hardware.soundtrigger@2.0-impl
 	
 # Bluetooth
 PRODUCT_PACKAGES += \
@@ -82,5 +83,3 @@ PRODUCT_PACKAGES += \
 # WiFi
 PRODUCT_PACKAGES += \
 	android.hardware.wifi@1.0-service
-  
-
