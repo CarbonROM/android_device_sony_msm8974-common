9e53c40a7da6ebb951282fed2b6a037ff24a0b17
Alexander Diewald <Diewi@diewald-net.com>
2018-01-06 11:20:02 +0100

    sony-8974: GPS: Update GPS HAL to sdm845 and loc_api.
    * Use the sdm845 GPS HAL instead of the msm8994 HAL that
      is also operational for msm8974.
    * Use the GPS treble service instead of the deprecated
      GPS HAL library.
    * Update the location API package list to comply to the
      recent version of the hardware/sony/location project.
    
    Change-Id: Ieb3b9476a5ca7330b59efd8aa0015cb5b65bdfc3
    Signed-off-by: Alexander Diewald <Diewi@diewald-net.com>

--- Android.mk
+++ Android.mk
@@ -3,6 +3,6 @@ LOCAL_PATH := $(call my-dir)
 ifeq ($(BOARD_VENDOR),sony)
 ifeq ($(TARGET_BOARD_PLATFORM),msm8974)
     include $(call all-subdir-makefiles,$(LOCAL_PATH))
-    include hardware/qcom/gps/msm8994/Android.mk
+    include hardware/qcom/gps/sdm845/Android.mk
 endif
 endif
