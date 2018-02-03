6a660c27a84f0e92532e7aa048e436055f8cee47
Chris Fries <cfries@google.com>
2018-02-03 02:35:38 -0600

    msm8974-common: gps: Switch to android.hardware.gnss@1.1
    follows: this in hardware/qcom/sdm845/gps
    
    commit d86de827449a525ea30d71537f1c08fe9514f23b
    Author: Baili Feng <bailif@codeaurora.org>
    Date:   Mon Jan 8 16:21:29 2018 +0800
    
        hidl gnss hal 1.1 implementation
    
        Implement new apis for IGnss 1.1
    
        Change-Id: I2c98c32355f6d9681d51b709697e49ec735fe1cf
        CRs-fixed: 2133458
    
    Change-Id: I100cbaf6e1c1f8ec7ae385628d542c79f46c93cf

--- hidl.mk
+++ hidl.mk
@@ -36,7 +36,8 @@ PRODUCT_PACKAGES += \
 
 # GPS
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.0-impl
+    android.hardware.gnss@1.1-impl-qti \
+    android.hardware.gnss@1.1-service-qti
 
 # Health
 PRODUCT_PACKAGES += \
