1f81aa66c406398c7be51d3f23ab3b1dc949fea2
Nikhil Punathil <nikhilpe@gmail.com>
2018-08-17 03:23:48 +0100

    msm8974-common: Nuke SEPolicy
    Change-Id: Iddca9be3410bf2a016949dca4cfef3e54e69861a
    Signed-off-by: Nikhil Punathil <nikhilpe@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -131,10 +131,10 @@ TARGET_USES_INTERACTION_BOOST := true
 TARGET_RIL_VARIANT := caf
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
+#include device/qcom/sepolicy/sepolicy.mk
 
-BOARD_SEPOLICY_DIRS += \
-    device/sony/msm8974-common/sepolicy
+#BOARD_SEPOLICY_DIRS += \
+#    device/sony/msm8974-common/sepolicy
 
 # Treble
 DEVICE_MANIFEST_FILE := device/sony/msm8974-common/treble-manifest.xml
